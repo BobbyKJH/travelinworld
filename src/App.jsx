953dbefac48204d77bcd05574977a21d36f78f32
BobbyKJH <rlatnguscjsw@gmail.com>
2022-05-28 19:21:51 +0900

    Travel in World Css in JS , React , React-Router-Dom let's start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InputSample from "./InputSample";
+
+function App() {
+  return (
+    <div className="App">
+      <InputSample />
+    </div>
+  );
+}
+
+export default App;
